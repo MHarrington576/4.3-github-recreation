310a755fde6f7465a63ef1eeeb1a4a937b6cb1ae
Matthew Harrington <mattharrington576@gmail.com>
2016-10-10 00:00:22 -0400

    Nearing completion

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,10 +3,71 @@ var _ = require('underscore');
 var Handlebars = require('handlebars');
 var githubToken = require('./gitapikey.js');
 
-if(githubToken !== undefined){
-  $.ajaxSetup({
-    headers: {
-      'Authorization': 'token ' + githubToken.token;
-    }
-  })
+
+$.ajax('https://api.github.com/users/MHarrington576').then(displayThisAttributeList);
+
+function displayThisAttributeList(data){
+  console.log(data);
+
+  var profileAttributes = data; // data is a single object in this case
+  var $profileAttributeContainer = $('.profile-nav'); // html container where data will be appended
+
+  var profileSource = $('#profile-nav-template').html(); // converts template into html
+  var profileTemplate = Handlebars.compile(profileSource); // compiles a template so it can be executed immediately
+
+  $profileAttributeContainer.append(profileTemplate(profileAttributes));
+}
+
+$.ajax('https://api.github.com/users/MHarrington576/orgs').then(displayOrganizations);
+
+function displayOrganizations(data){
+  var organizations = data;
+  var $orgContainer = $('.org-storage');
+
+  var orgSource = $('#organizations-template').html();
+  var orgTemplate = Handlebars.compile(orgSource);
+
+  _.each(organizations, function(org){
+    $orgContainer.append(orgTemplate(org));
+  });
+}
+
+
+
+$.ajax('https://api.github.com/users/MHarrington576').then(displayAttributeList);
+
+function displayAttributeList(data){
+  var attributes = data;
+  var $attributeContainer = $('.sidebar');
+
+  var source = $('#sidebar-template').html();
+  var template = Handlebars.compile(source);
+
+  $attributeContainer.append(template(attributes));
+}
+
+
+$.ajax('https://api.github.com/users/MHarrington576/repos').then(displayRepos);
+
+function displayRepos(data){
+  console.log(data);
+
+  var repos = data;
+  var $repoContainer = $('#repository-list');
+
+  var source = $('#repos-template').html();
+  var template = Handlebars.compile(source);
+
+  _.each(repos, function(repo){
+    $repoContainer.append(template(repo));
+  });
+}
+
+
+if (githubToken !== undefined) {
+    $.ajaxSetup({
+        headers: {
+            'Authorization': 'token ' + githubToken.token
+        }
+    });
 }
